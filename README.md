57b8d4c2f65c12e9e3b2008f0ba29bda682b48e3
liz in company <396635210@qq.com>
2018-12-13 16:30:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## QiNiu OSS(七牛云对象存储) Adapter For Flysystem.
+Flysystem 适配器： [七牛云](https://www.qiniu.com/)
+
+## Installation
+composer require liz/flysystem-qiniu
+
+## Usage
+```php
+require 'vendor/autoload.php';
+
+
+use League\Flysystem\Filesystem;
+use Liz\Flysystem\QiNiu\QiNiuOssAdapter;
+
+$cdnHost = 'http://default.cdn.host/'; # cdn [外链默认域名须英文](https://developer.qiniu.com/kodo/kb/5158/how-to-transition-from-test-domain-name-to-a-custom-domain-name)
+$bucket = 'bucket'; 
+$accessKey = 'access-key';
+$secretKey = 'secret-key';
+
+// write file
+$result = $flysystem->write('bucket/path/file.txt', 'contents');
+
+// write stream
+$stream = fopen('.env', 'r+');
+$result = $flysystem->writeStream('bucket/path/filestream.txt', $stream);
+
+// update file
+$result = $flysystem->update('bucket/path/file.txt', 'new contents');
+
+// has file
+$result = $flysystem->has('bucket/path/file.txt');
+
+// read file
+$result = $flysystem->read('bucket/path/file.txt');
+
+// delete file
+$result = $flysystem->delete('bucket/path/file.txt');
+
+// rename files
+$result = $flysystem->rename('bucket/path/filename.txt', 'bucket/path/newname.txt');
+
+// copy files
+$result = $flysystem->copy('bucket/path/file.txt', 'bucket/path/file_copy.txt');
+
+// list the contents
+$result = $flysystem->listContents('path', false);
+```
+
+## Notice
+由于七牛云没有文件夹的概念，建议顶级目录同bucket名
+`getVisibility()`,`setVisibility()`七牛云没有提供相关操作
---
+++ src/QiNiuOssAdapter.php
@@ -0,0 +1,435 @@
+<?php
+
+namespace Liz\Flysystem\QiNiu;
+
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\Config;
+use Qiniu\Auth;
+use Qiniu\Http\Client;
+use Qiniu\Http\Error;
+use Qiniu\Storage\BucketManager;
+use Qiniu\Storage\UploadManager;
+
+class QiNiuOssAdapter extends AbstractAdapter
+{
+    private $client;
+    private $auth;
+    private $bucket;
+    private $host;
+    private $bucketManager;
+    private $uploadManager;
+
+    /**
+     * QiNiuOssAdapter constructor.
+     *
+     * @param string $cdnHost
+     * @param string $bucket
+     * @param string $accessKey
+     * @param string $secretKey
+     */
+    public function __construct($cdnHost, $bucket, $accessKey, $secretKey)
+    {
+        $this->host = strripos($cdnHost, '/') + 1 === strlen($cdnHost) ? $cdnHost : $cdnHost.'/';
+        $this->bucket = $bucket;
+        $this->auth = new Auth($accessKey, $secretKey);
+        $this->client = new Client();
+    }
+
+    /**
+     * @return BucketManager
+     */
+    private function getBucketManager()
+    {
+        if (!$this->bucketManager) {
+            $this->bucketManager = new BucketManager($this->auth);
+        }
+        return $this->bucketManager;
+    }
+
+    /**
+     * @return UploadManager
+     */
+    private function getUploadManager()
+    {
+        if (!$this->uploadManager) {
+            $this->uploadManager = new UploadManager();
+        }
+        return $this->uploadManager;
+    }
+
+    /**
+     * @param null|Error $error
+     */
+    private function createExceptionIfError($error = null)
+    {
+        if ($error instanceof Error) {
+            $e = new QiNiuOssAdapterException($error->message(), $error->code());
+            throw $e->setResponse($error->getResponse());
+        }
+    }
+
+    /**
+     * @param array $response
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    private function ossResponse(array &$response)
+    {
+        if ($response[1] instanceof Error) {
+            $error = $response['1'];
+            $this->createExceptionIfError($error);
+        }
+        $response = $response[0];
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    private function urlEncode($path)
+    {
+        return strtr($path, [
+            ' ' => '%20',
+        ]);
+    }
+
+    /**
+     * @param string $path
+     * @param array  $normalized
+     *
+     * @return array
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    protected function getFileMeta($path, array $normalized)
+    {
+        $response = $this->getBucketManager()->stat($this->bucket, $path);
+        $this->ossResponse($response);
+
+        $normalized['mimetype'] = $response['mimeType'];
+        $normalized['timestamp'] = (int) ceil($response['putTime'] / 1000 / 10000);
+        $normalized['size'] = $response['fsize'];
+        return $normalized;
+    }
+
+    /**
+     * @param string $path
+     * @param bool   $requireMeta
+     * @param array  $options
+     *
+     * @return array|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    protected function mapFileInfo($path, $requireMeta = false, $options = [])
+    {
+        $normalized = [
+            'type' => 'file',
+            'path' => $path,
+        ];
+
+        if ($requireMeta) {
+            $normalized = $this->getFileMeta($path, $normalized);
+        }
+        $normalized = array_merge($normalized, $options);
+        return $normalized;
+    }
+
+    /**
+     * @param string $dirname
+     *
+     * @return array
+     */
+    protected function mapDirInfo($dirname)
+    {
+        $normalized = ['path' => $dirname, 'type' => 'dir'];
+        return $normalized;
+    }
+
+    /**
+     * @param string $path
+     * @param string $contents
+     * @param Config $config
+     *
+     * @return array|false
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function write($path, $contents, Config $config)
+    {
+        $uploadToken = $this->auth->uploadToken($this->bucket);
+        $response = $this->getUploadManager()->put($uploadToken, $path, $contents);
+        $this->ossResponse($response);
+
+        $fileInfo = $this->mapFileInfo($path, true, ['contents' => $contents]);
+        return $fileInfo;
+    }
+
+    /**
+     * @param string   $path
+     * @param resource $resource
+     * @param Config   $config
+     *
+     * @return array|false
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        return $this->write($path, stream_get_contents($resource), $config);
+    }
+
+    /**
+     * @param string $path
+     * @param string $contents
+     * @param Config $config
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function update($path, $contents, Config $config)
+    {
+        $uploadToken = $this->auth->uploadToken($this->bucket, $path);
+        $response = $this->getUploadManager()->put($uploadToken, $path, $contents);
+        $this->ossResponse($response);
+        $fileInfo = $this->mapFileInfo($path);
+        return $fileInfo;
+    }
+
+    /**
+     * @param string   $path
+     * @param resource $resource
+     * @param Config   $config
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        $uploadToken = $this->auth->uploadToken($this->bucket, $path);
+        $response = $this->getUploadManager()->put($uploadToken, $path, stream_get_contents($resource));
+        $this->ossResponse($response);
+        $fileInfo = $this->mapFileInfo($path);
+        return $fileInfo;
+    }
+
+    /**
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        $error = $this->getBucketManager()->rename($this->bucket, $path, $newpath);
+        $this->createExceptionIfError($error);
+        return !$error;
+    }
+
+    /**
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function copy($path, $newpath)
+    {
+        $error = $this->getBucketManager()->copy($this->bucket, $path, $this->bucket, $newpath);
+        $this->createExceptionIfError($error);
+        return !$error;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function delete($path)
+    {
+        $response = $this->getBucketManager()->delete($this->bucket, $path);
+        return !$response;
+    }
+
+    /**
+     * @param string $dirname
+     *
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        return true;
+    }
+
+    /**
+     * @param string $dirname
+     * @param Config $config
+     *
+     * @return array|false
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function createDir($dirname, Config $config)
+    {
+        $this->write($dirname.'/.init', 'hello world', $config);
+        $dirInfo = $this->mapDirInfo($dirname);
+        return $dirInfo;
+    }
+
+    /**
+     * @param string $path
+     * @param string $visibility
+     *
+     * @return array|false|void
+     */
+    public function setVisibility($path, $visibility)
+    {
+        // TODO: Implement setVisibility() method.  七牛云没有此功能
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|bool|null
+     */
+    public function has($path)
+    {
+        $response = $this->getBucketManager()->stat($this->bucket, $path);
+        return !$response[1];
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function read($path)
+    {
+        $response = $this->getBucketManager()->stat($this->bucket, $path);
+        $this->ossResponse($response);
+        $path = $this->urlEncode($path);
+        $result = $this->mapFileInfo($path, false, [
+            'contents' => file_get_contents($this->host.$path),
+        ]);
+        return $result;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function readStream($path)
+    {
+        $path = $this->urlEncode($path);
+        $stream = fopen($this->host.$path, 'rb');
+        $result = $this->mapFileInfo($path, false, ['stream' => $stream]);
+        return $result;
+    }
+
+    /**
+     * @param string $directory
+     * @param bool   $recursive
+     *
+     * @return array
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        $directory = $recursive ? '' : $directory;
+        $response = $this->getBucketManager()->listFiles($this->bucket);
+        $this->ossResponse($response);
+        $getDir = function ($path, $currentDir) {
+            $tmp = strtr($path, [
+                $currentDir.'/' => '',
+            ]);
+            $dir = substr($tmp, 0, stripos($tmp, '/'));
+            return $dir;
+        };
+        $files = $response['items'] ?: [];
+        $results = [];
+        foreach ($files as $file) {
+            $dir = $getDir($file['key'], $directory);
+            if ($dir) {
+                $result = $this->mapDirInfo($directory.'/'.$dir);
+            } else {
+                $result = $this->mapFileInfo($file['key'], false, [
+                    'timestamp' => (int) ceil($file['putTime'] / 1000 / 10000),
+                    'size' => $file['fsize'],
+                ]);
+            }
+            $results[] = $result;
+        }
+        $results = array_unique($results, SORT_REGULAR);
+        return $results;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function getMetadata($path)
+    {
+        $metaData = $this->mapFileInfo($path, true);
+        return $metaData;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function getSize($path)
+    {
+        $metaData = $this->getMetadata($path);
+        return $metaData['size'];
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function getMimetype($path)
+    {
+        $metaData = $this->getMetadata($path);
+        return $metaData;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|mixed
+     *
+     * @throws QiNiuOssAdapterException
+     */
+    public function getTimestamp($path)
+    {
+        $metaData = $this->getMetadata($path);
+        return $metaData['timestamp'];
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array|false|void
+     */
+    public function getVisibility($path)
+    {
+        // TODO: Implement getVisibility() method. 七牛云没有此功能
+    }
+}
